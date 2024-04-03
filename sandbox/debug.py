a2c5fd1aba7c60907996650f0b77e94ebc537e4e
RadoTheProgrammer <rado@arazakar.com>
2024-04-03 15:50:45 +0200

    commit

--- sandbox/debug.py
+++ sandbox/debug.py
@@ -1,9 +1,14 @@
+# %%
 
-from sympy import use
 from mcserial import MinecraftSerial
-from mcserial.mcserial import t
+#from mcpi.timer import Timer
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 mcs = MinecraftSerial(mc)
-t.enabled=True
-data=mcs.copy((102,1,15),(106,5,11),use_tqdm=True)
+
+
+# %%
+
+data=mcs.load("house.npy")
+# %%
+mcs.paste(data)
